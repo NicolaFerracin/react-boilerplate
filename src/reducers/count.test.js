c87c3ee56b998e9d1f00d5b52a3ef897a855f50a
NicolaFerracin <nicola.ferracin@gmail.com>
2017-08-11 20:24:44 +0200

    Rename counter to count

--- src/reducers/count.test.js
+++ src/reducers/count.test.js
@@ -1,4 +1,4 @@
-import counter from './count';
+import count from './count';
 import { increase, decrease } from '../actions/count';
 import { INCREASE, DECREASE } from '../constants';
 
@@ -6,19 +6,19 @@ let state = undefined;
 describe('reducers', () => {
   describe('counter', () => {
     it('should provide the initial state', () => {
-        state = counter( state, { type: undefined });
+        state = count( state, { type: undefined });
         expect(state.number).toBe(1)
     });
     it('should handle INCREASE action', () => {
-        state = counter(state, { type: INCREASE, amount: 1 });
+        state = count(state, { type: INCREASE, amount: 1 });
         expect(state.number).toBe(2)
     })
     it('should handle DECREASE action', () => {
-        state = counter(state, { type: DECREASE, amount: 1 });
+        state = count(state, { type: DECREASE, amount: 1 });
         expect(state.number).toBe(1);
     });
     it('should ignore unknown actions', () => {
-        state = counter(state, { type: 'unknown', amount: null });
+        state = count(state, { type: 'unknown', amount: null });
         expect(state.number).toBe(1)
     });
   });
