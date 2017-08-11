c058187a9ff3b1dfcf83074e090d0ae4788fd308
NicolaFerracin <nicola.ferracin@gmail.com>
2017-08-11 21:59:05 +0200

    Fix linting

--- src/actions/count.test.js
+++ src/actions/count.test.js
@@ -2,36 +2,36 @@ import { increase, decrease } from './count';
 import { INCREASE, DECREASE } from '../constants';
 
 describe('actions', () => {
-    describe('count', () => {
-        it('should run INCREASE action', () => {
-            const amount = 1000;
-            const expected = { 'type': INCREASE, amount };
-            expect(increase(amount)).toEqual(expected)
-        });
-        it('should run DECREASE action', () => {
-            const amount = 1000;
-            const expected = { 'type': DECREASE, amount };
-            expect(decrease(amount)).toEqual(expected)
-        });
-        it('should run INCREASE with string amount', () => {
-            const amount = 'abcd';
-            const expected = { 'type': INCREASE, amount };
-            expect(increase(amount)).toEqual(expected)
-        });
-        it('should run DECREASE with string amount', () => {
-            const amount = 'abcd';
-            const expected = { 'type': DECREASE, amount };
-            expect(decrease(amount)).toEqual(expected)
-        });
-        it('should run INCREASE with null amount', () => {
-            const amount = null;
-            const expected = { 'type': INCREASE, amount };
-            expect(increase(amount)).toEqual(expected)
-        });
-        it('should run DECREASE with string amount', () => {
-            const amount = null;
-            const expected = { 'type': DECREASE, amount };
-            expect(decrease(amount)).toEqual(expected)
-        });
+  describe('count', () => {
+    it('should run INCREASE action', () => {
+      const amount = 1000;
+      const expected = { 'type': INCREASE, amount };
+      expect(increase(amount)).toEqual(expected);
     });
+    it('should run DECREASE action', () => {
+      const amount = 1000;
+      const expected = { 'type': DECREASE, amount };
+      expect(decrease(amount)).toEqual(expected);
+    });
+    it('should run INCREASE with string amount', () => {
+      const amount = 'abcd';
+      const expected = { 'type': INCREASE, amount };
+      expect(increase(amount)).toEqual(expected);
+    });
+    it('should run DECREASE with string amount', () => {
+      const amount = 'abcd';
+      const expected = { 'type': DECREASE, amount };
+      expect(decrease(amount)).toEqual(expected);
+    });
+    it('should run INCREASE with null amount', () => {
+      const amount = null;
+      const expected = { 'type': INCREASE, amount };
+      expect(increase(amount)).toEqual(expected);
+    });
+    it('should run DECREASE with string amount', () => {
+      const amount = null;
+      const expected = { 'type': DECREASE, amount };
+      expect(decrease(amount)).toEqual(expected);
+    });
+  });
 });
--- src/reducers/count.test.js
+++ src/reducers/count.test.js
@@ -1,25 +1,24 @@
 import count from './count';
-import { increase, decrease } from '../actions/count';
 import { INCREASE, DECREASE } from '../constants';
 
-let state = undefined;
+let state;
 describe('reducers', () => {
   describe('count', () => {
     it('should provide the initial state', () => {
-        state = count( state, { type: undefined });
-        expect(state.number).toBe(1)
+      state = count(state, { 'type': null });
+      expect(state.number).toBe(1);
     });
     it('should handle INCREASE action', () => {
-        state = count(state, { type: INCREASE, amount: 1 });
-        expect(state.number).toBe(2)
-    })
+      state = count(state, { 'type': INCREASE, 'amount': 1 });
+      expect(state.number).toBe(2);
+    });
     it('should handle DECREASE action', () => {
-        state = count(state, { type: DECREASE, amount: 1 });
-        expect(state.number).toBe(1);
+      state = count(state, { 'type': DECREASE, 'amount': 1 });
+      expect(state.number).toBe(1);
     });
     it('should ignore unknown actions', () => {
-        state = count(state, { type: 'unknown', amount: null });
-        expect(state.number).toBe(1)
+      state = count(state, { 'type': 'unknown', 'amount': null });
+      expect(state.number).toBe(1);
     });
   });
 });
